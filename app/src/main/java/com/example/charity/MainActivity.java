44c18e0652f4711d89c7e58c573716436a7cf2b9
AhmedGsm <Ahmed.gsm1983@gmail.com>
2020-04-27 00:56:21 +0100

    Create landing activity
    Create landing activity
    
    Create landing activity

--- app/src/main/java/com/example/charity/MainActivity.java
+++ app/src/main/java/com/example/charity/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.charity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    // Get search benefactors button reference
+    @BindView(R.id.search_benefactor_btn) Button searchBenefactorBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind butterKnife library to activity
+        ButterKnife.bind(this);
+
+        // Set click listener for button to go to search activity
+        searchBenefactorBtn.setOnClickListener(view -> {
+            Intent intentToSearchActivity = new Intent(MainActivity.this,BenefactorsFindingActivity.class);
+            // Start activity with explicit intent
+            startActivity(intentToSearchActivity);
+        });
+
     }
 }
