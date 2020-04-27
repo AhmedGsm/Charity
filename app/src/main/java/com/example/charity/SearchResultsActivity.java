88df489fc7109b81cc9527b2741760fc3cdfa9df
Ahmed KHABER <Ahmed.gsm1983@gmail.com>
2020-04-27 19:28:52 +0100

    Create search results activity

--- app/src/main/java/com/example/charity/SearchResultsActivity.java
+++ app/src/main/java/com/example/charity/SearchResultsActivity.java
@@ -1,14 +1,28 @@
 package com.example.charity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchResultsActivity extends AppCompatActivity {
+
+    // Get search benefactors button reference
+    @BindView(R.id.stores_list_recycler_view) RecyclerView mRecyclerView;
+    @BindView(R.id.save_stores_list_button) Button  saveListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
     }
+
 }
