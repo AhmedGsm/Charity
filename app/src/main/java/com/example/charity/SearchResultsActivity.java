5b8a1788d17a0bf411baa87cec52fd6a0f3b4cba
Ahmed KHABER <Ahmed.gsm1983@gmail.com>
2020-04-27 19:31:29 +0100

    Setup recycler view for search results activity

--- app/src/main/java/com/example/charity/SearchResultsActivity.java
+++ app/src/main/java/com/example/charity/SearchResultsActivity.java
@@ -17,12 +17,26 @@ public class SearchResultsActivity extends AppCompatActivity {
     // Get search benefactors button reference
     @BindView(R.id.stores_list_recycler_view) RecyclerView mRecyclerView;
     @BindView(R.id.save_stores_list_button) Button  saveListButton;
+    private StoreSearchAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
+        // Bind butterKnife library to activity
+        ButterKnife.bind(this);
+
+        // Receive intent from search results intent
+        Intent currentIntent= getIntent();
+        Bundle bundleFromSearchResultsActivity = currentIntent.getExtras();
+
+        // set up the recycler view
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new StoreSearchAdapter(SearchResultsActivity.this, null);
+        mRecyclerView.setAdapter(mAdapter);
+        // Display store found in recycler view
+        mAdapter.swapStores(bundleFromSearchResultsActivity);
     }
 
 }
