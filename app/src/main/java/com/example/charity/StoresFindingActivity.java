1af49233f8ef282724bab477111128f246570a70
Ahmed KHABER <Ahmed.gsm1983@gmail.com>
2020-04-28 06:14:25 +0100

    RECT: Fetch Places from google map server

--- app/src/main/java/com/example/charity/StoresFindingActivity.java
+++ app/src/main/java/com/example/charity/StoresFindingActivity.java
@@ -12,12 +12,17 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.charity.utilities.BenefactorsFindingUtils;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -41,13 +46,9 @@ public class StoresFindingActivity extends AppCompatActivity {
     private String API_KEY = API_KEY2;
     private int i = 1;
     public static final String PLACE_SEARCHED_BUNDLE = "places_budle";
-    public final String PLACES_IDS_EXTRA = "places_ids";;
+    public static final String PLACES_IDS_EXTRA = "places_ids";;
     public static final String PLACES_NAMES_EXTRA = "places_names";
     public static final String PLACES_ADDRESSES_EXTRA = "places_addresses";
-    private ArrayList<String> mPlacesNames = new ArrayList<>();
-    private ArrayList<String> mPlacesAddresses = new ArrayList<>();
-    private ArrayList<String> mPlacesIds = new ArrayList<>();;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,32 +100,77 @@ public class StoresFindingActivity extends AppCompatActivity {
             // Hide search View and display progress bar
             mSearchView.setVisibility(View.GONE);
             mProgressBar.setVisibility(View.VISIBLE);
-            // Find place list
-            List<AutocompletePrediction> placesFound = BenefactorsFindingUtils.findPlacesList(this, mPlacesClient, searchQuery);
-            if(placesFound != null && placesFound.size() > 0) {
-                Log.i(TAG, "Places found log: " + placesFound);
-                // Go to listing places activity
-                Intent intentToDetailsPlacesActivity = new Intent(StoresFindingActivity.this, SearchResultsActivity.class );
-                for(int i = 0; i < placesFound.size(); i++) {
-                    mPlacesIds.add(placesFound.get(i).getPlaceId());
-                    mPlacesNames.add(placesFound.get(i).getPrimaryText(null).toString());
-                    mPlacesAddresses.add(placesFound.get(i).getFullText(null).toString());
-                }
-                // Create bundle to store place name and addresses to send them with intent
-                Bundle bundlePlaceDetails = new Bundle();
-                bundlePlaceDetails.putStringArrayList(PLACES_IDS_EXTRA,mPlacesNames);
-                bundlePlaceDetails.putStringArrayList(PLACES_NAMES_EXTRA,mPlacesNames);
-                bundlePlaceDetails.putStringArrayList(PLACES_ADDRESSES_EXTRA,mPlacesNames);
-                intentToDetailsPlacesActivity.putExtras(bundlePlaceDetails);
-                startActivity(intentToDetailsPlacesActivity);
-            } else {
-                Log.i(TAG, getString(R.string.place_not_found_str));
-                Toast.makeText(this, getString(R.string.place_not_found_str),Toast.LENGTH_SHORT).show();
-            }
-            // Display search view and hide progress
-            mProgressBar.setVisibility(View.GONE);
-            mSearchView.setVisibility(View.VISIBLE);
+            // Launch places finding request
+            this.findPlacesList(this, mPlacesClient, searchQuery);
         }
+    }
+
+    /**
+     *  Finds benefactors places location from google server
+     *
+     * @param context       Activity context
+     * @param placesClient  PlacesClient initialized in Activity
+     * @param searchQuery   Query entered from user
+     */
+
+    public void findPlacesList(Context context, PlacesClient placesClient, String searchQuery) {
+
+        // Create a new token for the autocomplete session.
+        AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
+        // Create a dummy RectangularBounds object for testing purposes.
+        RectangularBounds bounds = RectangularBounds.newInstance(
+                new LatLng(-33.880490, 151.184363),
+                new LatLng(-33.858754, 151.229596));
+        // Create a FindAutocompletePredictionsRequest instance.
+        FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+                .setLocationRestriction(bounds)
+                .setOrigin(new LatLng(28.0339, 1.6596 ))
+                .setSessionToken(token)
+                .setQuery(searchQuery)
+                .build();
+        // Set findAutocompletePredictions
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener(
+                findAutocompletePredictionsResponse -> {
+                    ArrayList<String> placesIds = new ArrayList<>();;
+                     ArrayList<String> placesNames = new ArrayList<>();
+                     ArrayList<String> placesAddresses = new ArrayList<>();
+                     List<AutocompletePrediction> mPlaces = new ArrayList<>();
+                    mPlaces =
+                            findAutocompletePredictionsResponse.getAutocompletePredictions();
+                    //
+                    if( mPlaces.size() > 0) {
+                        Log.i(TAG, "Places found : " + mPlaces);
+                        // Go to listing places activity
+                        Intent intentToDetailsPlacesActivity = new Intent(StoresFindingActivity.this, SearchResultsActivity.class );
+                        for(int i = 0; i < mPlaces.size(); i++) {
+                            placesIds.add(mPlaces.get(i).getPlaceId());
+                            placesNames.add(mPlaces.get(i).getPrimaryText(null).toString());
+                            placesAddresses.add(mPlaces.get(i).getSecondaryText(null).toString());
+                        }
+                        // Create bundle to store place name and addresses to send them with intent
+                        Bundle bundlePlaceDetails = new Bundle();
+                        bundlePlaceDetails.putStringArrayList(PLACES_IDS_EXTRA,placesIds);
+                        bundlePlaceDetails.putStringArrayList(PLACES_NAMES_EXTRA,placesNames);
+                        bundlePlaceDetails.putStringArrayList(PLACES_ADDRESSES_EXTRA,placesAddresses);
+                        intentToDetailsPlacesActivity.putExtras(bundlePlaceDetails);
+                        startActivity(intentToDetailsPlacesActivity);
+                    } else {
+                        Log.i(TAG, getString(R.string.place_not_found_str));
+                        Toast.makeText(this, getString(R.string.place_not_found_str),Toast.LENGTH_SHORT).show();
+                    }
+                    // Display search view and hide progress
+                    mProgressBar.setVisibility(View.GONE);
+                    mSearchView.setVisibility(View.VISIBLE);
+                }
+        ).addOnFailureListener(exception -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                Log.e(TAG, context.getString(R.string.place_not_found_str) + apiException.getStatusCode() + " " + apiException.getStatusMessage());
+                Toast.makeText(context, context.getString(R.string.place_not_found_Api_error),Toast.LENGTH_SHORT).show();
+                // Display search view and hide progress
+                mProgressBar.setVisibility(View.GONE);
+                mSearchView.setVisibility(View.VISIBLE);            }
+        });
     }
 
 }
